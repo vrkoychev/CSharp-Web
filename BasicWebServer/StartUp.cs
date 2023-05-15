4c42fabcb19202c266593f559ab2a934daa927f1
Valentin Koychev <114136127+vrkoychev@users.noreply.github.com>
2023-05-15 10:27:59 +0300

    Wrote simple response which content visualized on the browser page

--- BasicWebServer/StartUp.cs
+++ BasicWebServer/StartUp.cs
@@ -2,6 +2,7 @@
 {
     using System.Net;
     using System.Net.Sockets;
+    using System.Text;
 
     public class StartUp
     {
@@ -16,6 +17,19 @@
             Console.WriteLine("Listening for requests...");
 
             var connection = serverListener.AcceptTcpClient();
+            var networkStream = connection.GetStream();
+            var content = "Hello from my server!";
+            var contentLength = Encoding.UTF8.GetByteCount(content);
+
+            var response = $@"HTTP/1.1 200 OK
+Content-Type: text/plain; charset=UTF-8
+Content-Length: {contentLength}
+
+{content}";
+
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+            networkStream.Write(responseBytes);
+            connection.Close();
         }
     }
 }
