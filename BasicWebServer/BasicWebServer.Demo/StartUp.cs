606e2429619e079f077d7ebf67e7d9946b3e2093
Valentin Koychev <114136127+vrkoychev@users.noreply.github.com>
2023-05-15 10:49:04 +0300

    Implemented HttpServer class

---
+++ BasicWebServer/BasicWebServer.Demo/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace BasicWebServer.Demo
+{
+    using BasicWebServer.Server;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var server = new HttpServer("127.0.0.1", 8080);
+            server.Start();
+        }
+    }
+}
---
+++ BasicWebServer/BasicWebServer.Server/HttpServer.cs
@@ -0,0 +1,56 @@
+﻿namespace BasicWebServer.Server
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+
+    public class HttpServer
+    {
+        private readonly IPAddress ipAdress;
+        private readonly int port;
+        private readonly TcpListener serverListener;
+
+        public HttpServer(string ipAdress, int port)
+        {
+            this.ipAdress = IPAddress.Parse(ipAdress);
+            this.port = port;
+
+            serverListener = new TcpListener(this.ipAdress, port);
+        }
+
+        public void Start()
+        {
+            serverListener.Start();
+
+            Console.WriteLine($"Server started on port {port}...");
+            Console.WriteLine("Listening for requests...");
+
+            while (true)
+            {
+                var connection = serverListener.AcceptTcpClient();
+
+                var networkStream = connection.GetStream();
+
+                WriteResponse(networkStream, "Hello from the server!");
+
+                connection.Close();
+            }
+        }
+
+        private void WriteResponse(NetworkStream networkStream, string message)
+        {
+            var contentLength = Encoding.UTF8.GetByteCount(message);
+
+            var response = $@"HTTP/1.1 200 OK
+Content-Type: text/plain; charset=UTF-8
+Content-Length: {contentLength}
+
+{message}";
+
+            var responseBytes = Encoding.UTF8.GetBytes(response);
+
+            networkStream.Write(responseBytes);
+        }
+    }
+}
