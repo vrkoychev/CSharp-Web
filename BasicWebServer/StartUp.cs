b03739d9e5d2aff28d22175ca2a06cb68ca82bbf
Valentin Koychev <114136127+vrkoychev@users.noreply.github.com>
2023-05-15 10:21:07 +0300

    Just starting with my implementation of Basic HTTP Server

---
+++ BasicWebServer/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace BasicWebServer
+{
+    using System.Net;
+    using System.Net.Sockets;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var ipAdress = IPAddress.Parse("127.0.0.1");
+            var port = 8080;
+            var serverListener = new TcpListener(ipAdress, port);
+            serverListener.Start();
+
+            Console.WriteLine($"Server started on port {port}.");
+            Console.WriteLine("Listening for requests...");
+
+            var connection = serverListener.AcceptTcpClient();
+        }
+    }
+}
